aa3817bba8da53a0bd424f4fbf894ba4fcb307c4
Gadel Khuzin <Khuzingadel2002@gmail.com>
2026-06-10 05:38:58 +0300

    logic code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import './training';
+import { Colors } from '../enums/Color';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,32 @@ import './training';
 })
 export class AppComponent {
   companyName: string = 'румтибет';
+
+  constructor() {
+    this.methodSaveDate();
+    this.saveVisitCount();
+  }
+
+  methodTakeColors(color: string): boolean {
+    if (color === Colors.RED || color === Colors.GREEN || color === Colors.BLUE) {
+      return true;
+    }
+    return false;
+  }
+
+  methodSaveDate(): void {
+    const date = new Date();
+    localStorage.setItem('lastVisit', JSON.stringify(date));
+  }
+
+  saveVisitCount(): void {
+    const visits = localStorage.getItem('visits');
+
+    if (visits) {
+      const count = Number(visits) + 1;
+      localStorage.setItem('visits', JSON.stringify(count));
+    } else {
+      localStorage.setItem('visits', '1');
+    }
+  }
 }
---
+++ src/enums/Color.ts
@@ -0,0 +1,5 @@
+export enum Colors {
+  RED = 'rgb(255, 0, 0)',
+  GREEN = 'rgb(0, 128, 0)',
+  BLUE = 'rgb(0, 0, 255)',
+}
