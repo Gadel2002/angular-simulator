306d1034cd82a273e46d6aabb0c3227862db66c3
Gadel Khuzin <Khuzingadel2002@gmail.com>
2026-08-17 04:12:26 +0300

    feat: implemented page layout, date picker, loading screen, and live input

---
+++ src/interfaces/IProgram.ts
@@ -0,0 +1,6 @@
+export interface IProgram {
+  id: number;
+  image: string;
+  title: string;
+  description: string;
+}
---
+++ src/interfaces/IProgramPhoto.ts
@@ -0,0 +1,5 @@
+export interface IProgramPhoto {
+  id: number;
+  image: string;
+  alt: string;
+}
